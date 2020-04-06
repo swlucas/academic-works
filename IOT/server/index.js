53e79b8c5136397cc49a97144c2f58766efdbe53
Lucas Pereira Santos <lucas@MacBook-Pro-de-Lucas.local>
2020-04-06 09:45:01 -0300

    Finish test

--- IOT/server/index.js
+++ IOT/server/index.js
@@ -1,39 +1,83 @@
 net = require("net");
-const PORT = process.env.PORT || 3000;
-const SERVER = { name: "Temperatura_L", status: "ATIVADO", time: 2000 };
+const lineReader = require("line-reader");
 
-const server = net.createServer(function(socket) {
-  // socket.write("Bem vindx\r\n");
+const PORT = process.env.PORT || 3000;
+
+const trucks = [];
+const containers = [];
+const trucks_connection = [];
+
+const server = net.createServer(function (socket) {
   handleState(socket);
-  // socket.on("data", function(data) {
-  //   console.log(data.toString());
-  // });
 });
 
-const handleState = socket => {
+const handleState = (socket) => {
   let connected = true;
-  socket.on("close", a => (connected = a));
-  socket.on("data", function(data) {
-    console.log(data.toString());
-    if (!connected) return;
-    const [clientName, command, serviceName] = data.toString().split(" ");
-    if (
-      command.toUpperCase() == "CONECTAR" &&
-      serviceName.trim() == SERVER.name
-    ) {
-      SERVER.status === "ATIVADO" &&
-        socket.write(
-          `${SERVER.name} ${SERVER.status} ${SERVER.time / 1000}\r\n`
-        );
-      setInterval(() => {
-        connected && socket.write(`${Math.floor(Math.random() * 40)}\r\n`);
-        // time += SERVER.time / 1000;
-      }, SERVER.time);
+  socket.on("close", (a) => (connected = a));
 
-      SERVER.status === "DESATIVADO" && socket.write("DESATIVADO");
-    }
+  socket.on("data", function (data) {
+    if (!connected) return;
+    const info = data.toString().split(" ");
+
+    info[0] === "LIVRE" && handleTruckQueue(info);
+    info[0] === "COLETA_FINALIZADA" && handleTruckQueue(info);
+    info[0] === "CHEIO" && handleContainerQueue(info);
   });
 };
+
+const handleTruckQueue = (info) => {
+  const [_, _id] = info;
+  const truck = trucks.find((truck) => truck._id == _id.trim());
+  !truck
+    ? trucks.push({ _id: _id.trim(), status: "LIVRE" })
+    : (truck.status = "LIVRE");
+
+  trucks.length !== 0 && handleContainerCollect();
+  console.log("CAMINHOES ==>", trucks);
+};
+
+const handleContainerQueue = (info) => {
+  const [_, _id] = info;
+  const container = containers.find((container) => container._id == _id.trim());
+  !container
+    ? containers.push({ _id: _id.trim(), status: "CHEIO" })
+    : (container.status = "CHEIO");
+
+  containers.length !== 0 && handleContainerCollect();
+  console.log("CONTAINERS ==>", containers);
+};
+
+const handleContainerCollect = () => {
+  const truck = trucks.find((truck) => truck.status == "LIVRE");
+  const container = containers.find(
+    (container) => container.status === "CHEIO"
+  );
+
+  truck && container && handleConnect(truck, container);
+};
+
+const handleConnect = (truck, container) => {
+  const { ip, port } = trucks_connection.find(
+    (truck_con) => truck_con._id == truck._id
+  );
+
+  const client = new net.Socket();
+  client.connect(port, ip, () => {
+    client.write(`COLETAR ${container._id}\r\n`);
+    containers.splice(0, 1);
+    trucks.splice(0, 1);
+    console.log(`${truck._id} COLETAR ${container._id}`);
+  });
+};
+
+const listDevices = () => {
+  lineReader.eachLine("trucks_connection.txt", function (line) {
+    const [_id, ip, port] = line.split(" ");
+    trucks_connection.push({ _id, ip, port });
+  });
+};
+
+listDevices();
 
 server.listen(PORT);
 console.log(`Chat server running at port ${PORT}`);
